e8db3904e334a1c394a2171f0291b8103747a628
Martin Szauer <mszauer@Martins-MacBook-Pro.local>
2017-03-13 17:24:55 -0700

    added db.php with dummy data

---
+++ includes/db.php
@@ -0,0 +1,13 @@
+<?php 
+$location = "localhost";
+$username = "Martin_CMS";
+$pass = "****";
+$dbName = 'database';
+$port = 3306;
+
+$connection = mysqli_connect($location,$username,$pass,$dbName,$port);
+
+// if($connection){
+//     echo "Connection complete";
+// }
+?>
